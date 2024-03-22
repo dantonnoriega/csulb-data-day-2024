c77daa80c650678b3226697037c9b104586464fd
Danton Noriega-Goodwin <dnoriegagoodwin@apple.com>
2024-03-22 02:30:17 -0700

    subtree is the way to go

--- _deploy-gh-pages.sh
+++ _deploy-gh-pages.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
-# Refresh / Create / Deploy gh-pages
 
+# Refresh / Create / Deploy gh-pages
 # 1. checkout orphaned branch; remove README and .gitignore
 git checkout --orphan gh-pages
-git rm --cached .gitignore README.md
 
 # 2. generate slides
-quarto render danton-csulb-data-day-2024.qmd --to revealjs
+quarto render danton-csulb-data-day-2024.qmd --to revealjs -o slides.html --output-dir docs
 
-# 3. add slides.html then commit
-git add -f slides.html danton-csulb-data-day-2024_files
-git commit -m "slides render $(date '+%FT%T')"
+# 3. add docs then commit
+git add -f docs
+git commit -m "slides render $(date '+%FT%T')" docs
 
 # 4. delete upstream `gh-pages`
 git push origin --delete gh-pages
 
 # 5. push changes and come back to `main`
-git push origin gh-pages
+git subtree push --prefix docs origin gh-pages
 git checkout main
 
 # 6. remove gh-pages local branch
